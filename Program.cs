fa8dc3b68af3f4664bde3c996c1b5c32cfde877e
19PHP01 <40120293+hieu521@users.noreply.github.com>
2021-12-29 21:07:48 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuanLySinhVien quanLySinhVien = new QuanLySinhVien();
+
+            while (true)
+            {
+                Console.WriteLine("\nCHUONG TRINH QUAN LY SINH VIEN C#");
+                Console.WriteLine("*************************MENU**************************");
+                Console.WriteLine("**  1. Add Student.                               **");
+                Console.WriteLine("**  2. Update student information by ID .          **");
+                Console.WriteLine("**  3. Remove student ID .                         **");
+                Console.WriteLine("**  4. Find students by name.                      **");
+                Console.WriteLine("** 5. Sort students by average (GPA).              **");
+                Console.WriteLine("** 6. Sort students by name.                        *");
+                Console.WriteLine("** 7. Sort students by ID.                         **");
+                Console.WriteLine("** 8. Display the list of students.                 **");
+                Console.WriteLine("** 0. Exit                   **");
+                Console.WriteLine("*******************************************************");
+                Console.Write("Nhap tuy chon: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("\n1. Them sinh vien.");
+                        quanLySinhVien.NhapSinhVien();
+                        Console.WriteLine("\nThem sinh vien thanh cong!");
+                        break;
+                    case 2:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            int id;
+                            Console.WriteLine("\n2. Cap nhat thong tin sinh vien. ");
+                            Console.Write("\nNhap ID: ");
+                            id = Convert.ToInt32(Console.ReadLine());
+                            quanLySinhVien.UpdateSinhVien(id);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 3:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            int id;
+                            Console.WriteLine("\n3. Xoa sinh vien.");
+                            Console.Write("\nNhap ID: ");
+                            id = Convert.ToInt32(Console.ReadLine());
+                            if (quanLySinhVien.DeleteById(id))
+                            {
+                                Console.WriteLine("\nSinh vien co id = {0} da bi xoa.", id);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 4:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            Console.WriteLine("\n4. Tim kiem sinh vien theo ten.");
+                            Console.Write("\nNhap ten de tim kiem: ");
+                            string name = Convert.ToString(Console.ReadLine());
+                            List<SinhVien> searchResult = quanLySinhVien.FindByName(name);
+                            quanLySinhVien.ShowSinhVien(searchResult);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 5:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            Console.WriteLine("\n5. Sap xep sinh vien theo diem trung binh (GPA).");
+                            quanLySinhVien.SortByDiemTB();
+                            quanLySinhVien.ShowSinhVien(quanLySinhVien.getListSinhVien());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 6:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            Console.WriteLine("\n6. Sap xep sinh vien theo ten.");
+                            quanLySinhVien.SortByName();
+                            quanLySinhVien.ShowSinhVien(quanLySinhVien.getListSinhVien());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 7:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            Console.WriteLine("\n6. Sap xep sinh vien theo ID.");
+                            quanLySinhVien.SortByID();
+                            quanLySinhVien.ShowSinhVien(quanLySinhVien.getListSinhVien());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 8:
+                        if (quanLySinhVien.SoLuongSinhVien() > 0)
+                        {
+                            Console.WriteLine("\n7. Hien thi danh sach sinh vien.");
+                            quanLySinhVien.ShowSinhVien(quanLySinhVien.getListSinhVien());
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nSanh sach sinh vien trong!");
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("\nBan da chon thoat chuong trinh!");
+                        return;
+                    default:
+                        Console.WriteLine("\nKhong co chuc nang nay!");
+                        Console.WriteLine("\nHay chon chuc nang trong hop menu.");
+                        break;
+                }
+            }
+        }
+    }
+}
